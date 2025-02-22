1325b034636694c7d0987a54e9a830865130ce25
StevenScript <sdn709466@gmail.com>
2025-02-22 14:51:59 -0330

    ADD: Base File setup via Springboot Initializr

---
+++ src/main/java/com/example/concertManagement_Server/ConcertManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.concertManagement_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concertManagement_Server/ConcertManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concertManagement_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
