d8224adf18b54626916fd319a3d481f9db826426
StevenScript <sdn709466@gmail.com>
2025-02-23 11:44:24 -0330

    ADD: Create/Update Venue methods.

--- src/main/java/com/example/concertManagement_Server/service/VenueService.java
+++ src/main/java/com/example/concertManagement_Server/service/VenueService.java
@@ -19,4 +19,17 @@ public class VenueService {
         Optional<Venue> optional = venueRepository.findById(id);
         return optional.orElse(null);
     }
+
+    public Venue createVenue(Venue venue) {
+        return venueRepository.save(venue);
+    }
+
+    public Venue updateVenue(Long id, Venue updatedData) {
+        return venueRepository.findById(id).map(venue -> {
+            venue.setName(updatedData.getName());
+            venue.setLocation(updatedData.getLocation());
+            venue.setCapacity(updatedData.getCapacity());
+            return venueRepository.save(venue);
+        }).orElse(null);
+    }
 }
