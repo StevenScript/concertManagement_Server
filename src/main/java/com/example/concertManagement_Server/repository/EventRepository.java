83904b161a332073319dad6c9056dbb8d65c31f7
StevenScript <sdn709466@gmail.com>
2025-02-24 11:20:11 -0330

    ADD; Find Event By Artist Query
    findEventsByArtistId(Long artistId) in EventRepository

--- src/main/java/com/example/concertManagement_Server/repository/EventRepository.java
+++ src/main/java/com/example/concertManagement_Server/repository/EventRepository.java
@@ -2,8 +2,14 @@ package com.example.concertManagement_Server.repository;
 
 import com.example.concertManagement_Server.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT e FROM Event e JOIN e.artists a WHERE a.id = :artistId")
+    List<Event> findEventsByArtistId(Long artistId);
 }
