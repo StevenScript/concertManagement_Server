ca30f3621582bb803fb3e40df8033029eb00714a
StevenScript <sdn709466@gmail.com>
2025-04-11 03:22:20 -0230

    FIX - added H2 support for github CI

--- src/test/java/com/example/concertManagement_Server/ConcertManagementServerApplicationTests.java
+++ src/test/java/com/example/concertManagement_Server/ConcertManagementServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.concertManagement_Server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ConcertManagementServerApplicationTests {
 
 	@Test
